f1cb491b7fee443918a29ceb0bf422ec3db2a0bf
carmelogiuliano <carmelo.giuliano1@gmail.com>
2023-01-24 18:24:52 +1100

    Rocket movement

--- RocketBoost/Assets/Scripts/Movement.cs
+++ RocketBoost/Assets/Scripts/Movement.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationSpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
 
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    void ApplyRotation(float rotation)
+    {
+        transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
     }
 }
